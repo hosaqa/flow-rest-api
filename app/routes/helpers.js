6d43e766d3e09a09a981a301cbf47e82ebd6fa29
xhosaka <rayalwood@gmail.com>
2020-01-16 00:15:43 +0200

    add titles to playlists

--- app/routes/helpers.js
+++ app/routes/helpers.js
@@ -1,19 +1,21 @@
 const utils = require("../utils");
 const Track = require("../models/Track");
 
-
-const getPlaylist = ({req, res, findBy}) => {
-  Track.find({[findBy]: req.params.id})
+const getPlaylist = ({ req, res, findBy }) => {
+  Track.find({ [findBy]: req.params.id })
     .populate("artist")
     .populate("genre")
     .exec((err, playlist) => {
       if (err) {
-        res.status(500).send({ message: `${utils.capitalize(findBy)} not found.` });
+        res
+          .status(500)
+          .send({ message: `${utils.capitalize(findBy)} not found.` });
       } else {
         const host = `${req.protocol}://${req.get("host")}`;
 
         res.send({
           uri: `${findBy}:${req.params.id}`,
+          title: playlist[0][findBy].name,
           playlistData: playlist.map(track => {
             if (track.artist.img) {
               track.artist.img = utils.getAbsolutePath(host, track.artist.img);
@@ -28,8 +30,8 @@ const getPlaylist = ({req, res, findBy}) => {
         });
       }
     });
-}
+};
 
 module.exports = {
   getPlaylist
-}
+};
